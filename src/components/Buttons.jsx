3d3b208a675689c680562723f059cf9260029851
Suraj Sharma <suraj200delhi@gmail.com>
2024-09-20 23:53:29 +0530

    Change in Buttons component

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
+
 function Buttons() {
+
   return (
     <div className='flex flex-col w-full items-center gap-4'>
-      <WalletMultiButton className="w-full sm:w-auto" />
-      <WalletDisconnectButton  className="w-full sm:w-auto" />
+      <WalletMultiButton className="w-full sm:w-auto " />
+      <WalletDisconnectButton className="w-full sm:w-auto" />
     </div>
   );
 }
