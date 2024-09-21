785ab22b40305cd38c028e0fa89f892ca58b662a
Suraj Sharma <suraj200delhi@gmail.com>
2024-09-21 23:55:24 +0530

    Chane SendToken component

--- src/components/SendTokens.jsx
+++ src/components/SendTokens.jsx
@@ -1,45 +1,63 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
-import { LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction} from "@solana/web3.js";
+import { LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 
 
 export function SendTokens() {
-    const wallet = useWallet();
-    const {connection} = useConnection();
+  const wallet = useWallet();
+  const { connection } = useConnection();
+  const [amount, setAmount] = useState(0)
+  const [receiver, setReceiver] = useState("")
+  const amountHandler = (e) => {
+    const userinput = e.target.value;
+    if (userinput > 5) {
+      setAmount(5)
+      toast.error('Limit exceed: 5 Sol at a time')
+    } else {
 
-    async function sendTokens() {
-        let to = document.getElementById("to").value;
-        let amount = document.getElementById("amount").value;
-        const transaction = new Transaction();
-        transaction.add(SystemProgram.transfer({
-            fromPubkey: wallet.publicKey,
-            toPubkey: new PublicKey(to),
-            lamports: amount * LAMPORTS_PER_SOL,
-        }));
-
-        await wallet.sendTransaction(transaction, connection);
-        alert("Sent " + amount + " SOL to " + to);
+      setAmount(userinput);
     }
+  }
+  const receiverHandler = (e) => {
+    setReceiver(e.target.value)
+  }
+
+  async function sendTokens() {
+    const transaction = new Transaction();
+    transaction.add(SystemProgram.transfer({
+      fromPubkey: wallet.publicKey,
+      toPubkey: new PublicKey(receiver),
+      lamports: amount * LAMPORTS_PER_SOL,
+    }));
+
+    await wallet.sendTransaction(transaction, connection);
+    toast.success("Sent " + amount + " SOL to " + receiver);
+  }
 
   return (
-    <div className="flex flex-col w-full h-screen gap-2 text-black">
+    <div className="flex flex-col w-full  gap-2 text-black">
       <div className="flex flex-row w-full justify-between gap-2 ">
         <input
-          id="to"
+          value={receiver}
+          onChange={receiverHandler}
           type="text"
-          placeholder="To" 
+          placeholder="To"
           className="w-1/2 p-2 rounded-md "
-          
-          />
+
+        />
 
         <input
-          id="amount"
-          type="text"
-          placeholder="Amount" 
+          value={amount}
+          onChange={amountHandler}
+          type="number"
+          placeholder="Amount"
           className="w-1/2 p-2 rounded-md "
-          />
+        />
 
       </div>
-      <button className="bg-gray-500 rounded-md p-2 w-1/2" onClick={sendTokens}>Send</button>
+      <button className="bg-gray-500 rounded-md p-2  w-1/2" onClick={sendTokens}>Send</button>
     </div>
   );
 }
